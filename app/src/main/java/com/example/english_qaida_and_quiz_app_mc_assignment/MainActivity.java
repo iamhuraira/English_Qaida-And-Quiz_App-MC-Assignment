4b839a39577c765fbcd5b32d9c90d8e9b45682e1
Abu Huraira <iamhuraira429@gmail.com>
2022-05-21 23:08:33 +0500

    Write The code For Changing layout

--- app/src/main/java/com/example/english_qaida_and_quiz_app_mc_assignment/MainActivity.java
+++ app/src/main/java/com/example/english_qaida_and_quiz_app_mc_assignment/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.english_qaida_and_quiz_app_mc_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void abc_qaida(View view) {
+        Intent i = new Intent(this, abc_page.class);
+        startActivity(i);
     }
 }
