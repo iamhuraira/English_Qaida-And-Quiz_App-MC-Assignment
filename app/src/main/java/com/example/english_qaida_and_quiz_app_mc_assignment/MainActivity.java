52be8187b81dfdfc474f88f88ea54224a0375e0c
Abu Huraira <iamhuraira429@gmail.com>
2022-05-22 21:45:50 +0500

    Building The Quiz Section

--- app/src/main/java/com/example/english_qaida_and_quiz_app_mc_assignment/MainActivity.java
+++ app/src/main/java/com/example/english_qaida_and_quiz_app_mc_assignment/MainActivity.java
@@ -18,4 +18,9 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, abc_page.class);
         startActivity(i);
     }
+
+    public void quiz_app(View view) {
+        Intent i = new Intent(this, start_quiz.class);
+        startActivity(i);
+    }
 }
