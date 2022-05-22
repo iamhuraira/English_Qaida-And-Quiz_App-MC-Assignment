9747c7f8dfc47c2c3006774e6278193d3b040c11
Abu Huraira <iamhuraira429@gmail.com>
2022-05-22 15:21:12 +0500

    created the buttons onclick function and intent code for changing layout

--- app/src/main/java/com/example/english_qaida_and_quiz_app_mc_assignment/abc_page.java
+++ app/src/main/java/com/example/english_qaida_and_quiz_app_mc_assignment/abc_page.java
@@ -38,4 +38,109 @@ public class abc_page extends AppCompatActivity {
         Intent i = new Intent(this, E.class);
         startActivity(i);
     }
+
+    public void f_click(View view) {
+        Intent i = new Intent(this, f.class);
+        startActivity(i);
+    }
+
+    public void g_click(View view) {
+        Intent i = new Intent(this, g.class);
+        startActivity(i);
+    }
+
+    public void h_click(View view) {
+        Intent i = new Intent(this, h.class);
+        startActivity(i);
+    }
+
+    public void i_click(View view) {
+        Intent i = new Intent(this, i.class);
+        startActivity(i);
+    }
+
+    public void j_click(View view) {
+        Intent i = new Intent(this, j.class);
+        startActivity(i);
+    }
+
+    public void k_click(View view) {
+        Intent i = new Intent(this, k.class);
+        startActivity(i);
+    }
+
+    public void l_click(View view) {
+        Intent i = new Intent(this, l.class);
+        startActivity(i);
+    }
+
+    public void m_click(View view) {
+        Intent i = new Intent(this, m.class);
+        startActivity(i);
+    }
+
+    public void n_click(View view) {
+        Intent i = new Intent(this, n.class);
+        startActivity(i);
+    }
+
+    public void o_click(View view) {
+        Intent i = new Intent(this, o.class);
+        startActivity(i);
+    }
+
+    public void p_click(View view) {
+        Intent i = new Intent(this, p.class);
+        startActivity(i);
+    }
+
+    public void q_click(View view) {
+        Intent i = new Intent(this, q.class);
+        startActivity(i);
+    }
+
+    public void r_click(View view) {
+        Intent i = new Intent(this, r.class);
+        startActivity(i);
+    }
+
+    public void s_click(View view) {
+        Intent i = new Intent(this, s.class);
+        startActivity(i);
+    }
+
+    public void t_click(View view) {
+        Intent i = new Intent(this, t.class);
+        startActivity(i);
+    }
+
+    public void u_click(View view) {
+        Intent i = new Intent(this, u.class);
+        startActivity(i);
+    }
+
+//    public void v_click(View view) {
+//        Intent i = new Intent(this, v.class);
+//        startActivity(i);
+//    }
+//
+//    public void w_click(View view) {
+//        Intent i = new Intent(this, w.class);
+//        startActivity(i);
+//    }
+//
+//    public void x_click(View view) {
+//        Intent i = new Intent(this, x.class);
+//        startActivity(i);
+//    }
+//
+//    public void y_click(View view) {
+//        Intent i = new Intent(this, y.class);
+//        startActivity(i);
+//    }
+//
+//    public void z_click(View view) {
+//        Intent i = new Intent(this, z.class);
+//        startActivity(i);
+//    }
 }
