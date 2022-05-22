26150ddf2908ee59644cefca585161628b8166d5
Abu Huraira <iamhuraira429@gmail.com>
2022-05-22 13:20:54 +0500

    created onclick function on buttons

--- app/src/main/java/com/example/english_qaida_and_quiz_app_mc_assignment/abc_page.java
+++ app/src/main/java/com/example/english_qaida_and_quiz_app_mc_assignment/abc_page.java
@@ -2,7 +2,9 @@ package com.example.english_qaida_and_quiz_app_mc_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class abc_page extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class abc_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abc_page);
+    }
+
+    public void a_click(View view) {
+        Intent i = new Intent(this, a.class);
+        startActivity(i);
+    }
+
+    public void b_click(View view) {
+        Intent i = new Intent(this, b.class);
+        startActivity(i);
+    }
+
+    public void c_click(View view) {
+        Intent i = new Intent(this, c.class);
+        startActivity(i);
     }
 }
